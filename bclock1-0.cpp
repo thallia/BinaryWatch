d73b6b85389a0690c5c468d57b8055bc6f249766
thallia <thalliatree@gmail.com>
2018-01-28 19:19:45 -0800

    fixed the minute portion of the code
    replaced the if(millis() - lastTick) with the correct stuff, turns out that had been goofing the whole thing up. Now onto deep sleep.

--- bclock1-0.cpp
+++ bclock1-0.cpp
@@ -2,7 +2,7 @@
  * written by: thallia
  * almost copied from Gector's binary clock
  * on the day: 1-27-18
- * last edit: 1-27-18
+ * last edit: 1-28-18
  */
 
 /* ------ Variables ------ */
@@ -15,11 +15,13 @@ const int clockP = 3;
 const int latchP = 4;
 const int dataP = 5;
 
+const int clearAmount = 16;
+
 // time variables
 int second = 0;
-int minute = 0;
-int hour = 0;
-int halfDay = 0; // so you don't get military time
+int minute = 1;
+int hour = 1;
+int halfDay = 1; // so you don't get military time
 
 static unsigned long tick = 0;    // for millis()
 
@@ -39,7 +41,7 @@ void enableOutput(boolean state){
 }
 
 // clock, shift the dang registers already!
-void clock(int hold = wait){
+void ticktock(int hold = wait){
   digitalWrite(clockP, HIGH); //turns clock on 
   delay(hold);
   digitalWrite(clockP, LOW); // turns clock off
@@ -58,7 +60,7 @@ void latch(int hold = wait){
 void writeData(int hold = wait){
   digitalWrite(dataP, HIGH); // sets datapin ready to receive data
   latch(hold);               // latches new data
-  clock(hold);            // clocks data through shift register
+  ticktock(hold);            // clocks data through shift register
   digitalWrite(dataP, LOW);  // turns datapin off
 }
 
@@ -73,33 +75,28 @@ void hourDisplay(int hour){
         if(bit == 1){ // clock the shift registers + write the data to LEDs
           digitalWrite(dataP, HIGH);
           latch();
-          clock();
+          ticktock();
           digitalWrite(dataP, LOW);
     } else { // keep ticking
       latch();
-      clock();
+      ticktock();
       }
     }
   }
 }
 
 // display minutes
-
-/* This doesn't display correctly right now, I'll have to find another way to implement it to display the minutes in correct order.
-   Also should try moving the i/o pins on the shift registers to see how that makes it look. double check circuit.
-
-   the minutes should be on a 60 scale, so it counts up to 60, it only looks like it's counting up to 15 rn */ 
-void minDisplay(int minute){
-  for(int x = 3; x >= 0; x--){
-    int bit = bitRead(minute, x);
+void minDisplay(int m){
+  for(int x = 5; x >= 0; x--){
+    int bit = bitRead(m, x);
     if(bit == 1){
       digitalWrite(dataP, HIGH);
       latch();
-      clock();
+      ticktock();
       digitalWrite(dataP, LOW);
     } else {
       latch();
-      clock();
+      ticktock();
     }
   }
 }
@@ -110,11 +107,11 @@ void ampm(int state){
   if (state >= 1){
     digitalWrite(dataP, HIGH);
     latch();
-    clock();
+    ticktock();
     digitalWrite(dataP, LOW);
   } else {
     latch();
-    clock();
+    ticktock();
   }
 }
 
@@ -123,7 +120,7 @@ void ampm(int state){
 // apparently it's an arduino thing that setup() runs first, TIL
 
 void setup(){
-
+  Serial.begin(9600);
 
   // output pins
   pinMode(clockP, OUTPUT);
@@ -141,11 +138,16 @@ void setup(){
 // This will run repeatedly, after setup.
 
 void loop(){
+  Serial.print("Minutes: ");
+  Serial.println(minute);
 
   // timing
+  static unsigned long lastTick = 0;
+
   // one second every 1000ms
-  tick = millis();
-  if(millis() - tick >= 1000){
+ 
+  if(millis() - lastTick >= 1000){
+    lastTick = millis();
     second++;
   }
 
